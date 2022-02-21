e8606c2c3303b2a9c9122a5e6a8e64c0e117c03f
Khang Nguyen <nguyentruongkhang22@gmail.com>
2022-02-21 21:31:35 +0700

    main

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -47,7 +47,12 @@ exports.getTour = async (req, res) => {
                 tour,
             },
         });
-    } catch (error) {}
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            msg: error,
+        });
+    }
 };
 
 exports.createTour = async (req, res) => {
@@ -60,8 +65,9 @@ exports.createTour = async (req, res) => {
             },
         });
     } catch (error) {
-        res.status(400).json({
+        res.status(404).json({
             status: 'fail',
+            msg: error,
         });
     }
 };
@@ -94,6 +100,81 @@ exports.deleteTour = async (req, res) => {
         res.status(204).json({
             status: 'success',
             data: null,
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            msg: error,
+        });
+    }
+};
+
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte: 4.5 } },
+            },
+            {
+                $group: {
+                    _id: { $toUpper: '$difficulty' },
+                    num: { $sum: 1 },
+                    numRatings: { $sum: '$ratingsQuantity' },
+                    avgRating: { $avg: '$ratingsAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' },
+                },
+            },
+            {
+                $sort: {
+                    avgPrice: 1,
+                },
+            },
+            // {
+            //     $match: { _id: { $ne: 'easy' } },
+            // },
+        ]);
+        res.status(200).json({
+            status: 'success',
+            data: { stats },
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            msg: error,
+        });
+    }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+    try {
+        const year = req.params.year * 1;
+        const plan = await Tour.aggregate([
+            {
+                $unwind: '$startDates',
+            },
+            {
+                $match: {
+                    startDates: {
+                        $gte: new Date(`${2021}-01-01`),
+                        $lte: new Date(`${2021}-12-31`),
+                    },
+                },
+            },
+            {
+                $group: {
+                    _id: { $month: '$startDates' },
+                    numTourStarts: { $sum: 1 },
+                },
+            },
+        ]);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                quantity: plan.length,
+                data: plan,
+            },
         });
     } catch (error) {
         res.status(404).json({
